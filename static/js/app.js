54de147d83052d7cdbc9d2a1664da9413a637060
csaving <csaving78@att.net>
2023-12-19 14:56:25 -0600

    bar and bubble chart working

--- static/js/app.js
+++ static/js/app.js
@@ -7,12 +7,13 @@ function init() {
     names.forEach(nameId => {dropdownMenu.append("option").text(nameId).property("value")})
     console.log(names[0])
     optionChanged(names[0])
-    barChart(names[0])
   })
 };   
 
 function optionChanged(id) {
     metaData(id)
+    barChart(id)
+    bubbleChart(id)
 }
 
 // Function to populate demographic data
@@ -31,24 +32,35 @@ function metaData(id) {
   })
 }
 
+function otuString (id) {
+  let text = []
+  for (let i = 0; i < id.length; i++) {
+    text.push("OTU " + id[i]);
+  }
+  return text 
+}
+
 // Functionn to create horizontal bar chart
 function barChart (id) {
     let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
     d3.json(url).then(function(data) {
         console.log(data);
-        // let demoData = d3.select("#sample-metadata");
+
         let samples = data.samples;
         let filterData = samples.filter(samples => samples.id == id)
         console.log(filterData)
         let firstMeta = filterData[0]
-        let sampleValues = firstMeta.sample_values.slice(0,10)
-        let otuIds = firstMeta.otu_ids.slice(0,10)
-        let otuLabels = firstMeta.otu_labels.slice(0,10)
-        console.log(sampleValues)
+        let sampleValues = firstMeta.sample_values.slice(0,10).reverse()
+
+        // console.log(sampleValues)
+        let otuIds = firstMeta.otu_ids.slice(0,10).reverse()
+        let otuIdText = otuString(otuIds)
+        // console.log(otuIdText)
+        let otuLabels = firstMeta.otu_labels.slice(0,10).reverse()
 
         let barData = [{
             x: sampleValues,
-            y: otuIds,
+            y: otuIdText,
             text: otuLabels,
             type: "bar",
             orientation: "h"
@@ -57,6 +69,44 @@ function barChart (id) {
         Plotly.newPlot("bar", barData);
     })
 }
+
+function bubbleChart (id) {
+  let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+  d3.json(url).then(function(data) {
+      let samples = data.samples;
+      let filterData = samples.filter(samples => samples.id == id)
+      // console.log(filterData)
+      let firstMeta = filterData[0]
+      let sampleValues = firstMeta.sample_values.slice(0,10).reverse()
+
+      let otuIds = firstMeta.otu_ids.slice(0,10).reverse()
+
+      let otuLabels = firstMeta.otu_labels.slice(0,10).reverse()
+
+  let trace1 = {
+    x: otuIds,
+    y: sampleValues,
+    mode: 'markers',
+    marker: {
+      color: otuIds,
+      // opacity: [1, 0.8, 0.6, 0.4],
+      size: sampleValues
+    }
+  };
+  
+  let bubbleData = [trace1];
+  
+  // let layout = {
+  //   title: 'OTU Bubbles',
+  //   showlegend: false,
+  //   height: 600,
+  //   width: 600
+  // };
+
+  Plotly.newPlot('bubble', bubbleData);
+})
+}
+
 
 //Calling the init function
 init();
